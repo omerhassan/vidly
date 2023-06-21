4688c43aa077cce44706db321c3f20a47052359f
omerhassan <omerhassan688@gmail.com>
2023-06-21 20:17:30 +0400

    modeled the relationships between connected data

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 const express = require('express');
 const debug = require('debug')('app:startup');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to MongoDB...'))
@@ -18,6 +21,7 @@ debug('[Starting The Express App]')
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
